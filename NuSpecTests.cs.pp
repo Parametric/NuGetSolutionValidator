1a9d8611e800cb4d0584aeb18990a62b19cceb31
chrismc <chrismc@077270f5-3bd0-7742-9fc2-aea18276b555>
2012-08-02 19:01:43 +0000

    Updated the package to move the Test Configuration into a separate file.
    git-svn-id: svn://gary/repos/NuGetSolutionValidator.NUnit/trunk@53013 077270f5-3bd0-7742-9fc2-aea18276b555

--- NuSpecTests.cs.pp
+++ NuSpecTests.cs.pp
@@ -7,29 +7,15 @@ namespace $rootnamespace$.NuGet
     [TestFixture]
     public class NuSpecTests
     {
-        protected SolutionMetaData Solution { get; set; }
+        protected NuSpecTestConfiguration Configuration { get; set; }
+		protected SolutionMetaData Solution { get; set; }
 
-		const string SolutionName = "Your solution name here";
-		
         [TestFixtureSetUp]
         public void BeforeAnyTestRuns()
-		{
-		
-            this.Solution = new SolutionMetaData(SolutionName);
-
-            var nuspec1 = new NuSpecInfo
-            {
-                Name = "Your nuspec name here",
-                NuSpecFile = Solution.Directory.GetFiles("your nuspec file here").First(),
-                Projects = Solution.GetAllProductionProjects().Where(p => p.Name == "your project name here").ToArray(), // provide the list of projects included in the nuspec.
-                OptionalDependencies = new string[]
-				{
-					"OptionalPackage1",
-				}, // provide a list of packages that will not be required to be included
-            };
-
-            this.Solution.NuSpecFiles = new [] {nuspec1 }; // {nuspec1, nuspec2, nuspec3, nuspec4, ...)
-        }
+		{		
+            this.Configuration = new NuSpecTestConfiguration();
+			this.Solution = this.Configuration.Solution;
+		}
 
         [Test]
         public void NuSpecsAreWellDefined()
