da1f4f182b62a7a43a6e30bb813f90387b2bcf57
Josh Rolstad <jrolstad@gmail.com>
2013-03-15 14:35:46 -0700

    Issue #1: Separating NUnit and the validation pieces

---
+++ NuGetSolutionValidator/NuSpecTestConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+
+namespace NugetSolutionValidator// $rootnamespace$.NuGet
+{
+    public class NuSpecTestConfiguration
+    {
+        public SolutionMetaData Solution { get; set; }
+
+		const string SolutionName = "Your solution name here";
+		
+        public NuSpecTestConfiguration()
+		{
+            this.Solution = new SolutionMetaData(SolutionName);
+
+            var nuspec1 = new NuSpecInfo
+            {
+                Name = "Your nuspec name here",
+                NuSpecFile = Solution.Directory.GetFiles("your nuspec file here").First(),
+                Projects = Solution.GetAllProductionProjects().Where(p => p.Name == "your project name here").ToArray(), // provide the list of projects included in the nuspec.
+                OptionalDependencies = new string[]
+				{
+					"OptionalPackage1",
+				}, // provide a list of packages that will not be required to be included
+            };
+
+            this.Solution.NuSpecFiles = new [] {nuspec1 }; // {nuspec1, nuspec2, nuspec3, nuspec4, ...)
+        }
+
+    }
+}
